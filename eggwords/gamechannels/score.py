cec650ad5a21e73e7c0e563fe9fd836ec79a5077
Matt Kruszewski <m@ttkrusz.com>
2017-11-30 17:06:38 -0500

    Fix key lookup bug in scoring a game

--- eggwords/gamechannels/score.py
+++ eggwords/gamechannels/score.py
@@ -4,7 +4,7 @@ def score_game(game_state):
     score = dict(((pid, 0) for pid in game_state.player_ids))
     for (word, player_id) in game_state.used_words.items():
         try:
-            score[player_id] += SCORE_BY_LEN[len(word)]
+            score[player_id] = score.get(player_id, 0) + SCORE_BY_LEN[len(word)]
         except IndexError:
             if len(word) > len(SCORE_BY_LEN):
                 score[player_id] += SCORE_BY_LEN[-1]
