a4666b373b4577715080932f6a67508b6ef2a706
Matt Kruszewski <m@ttkrusz.com>
2017-12-07 10:31:40 -0700

    Remove initial dev SECRET_KEY

--- eggwords/eggwords/settings/base_settings.py
+++ eggwords/eggwords/settings/base_settings.py
@@ -15,18 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+s@8sxmvh7ny!)yk5sjh=pln!5e%hmdcvt+*zr-n*)(+)2$b2!'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['localhost', '66.175.218.7']
-
 # Application definition
 
 INSTALLED_APPS = [
