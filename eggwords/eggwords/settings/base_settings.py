4c0d3ee4f7319a7a0c4050ecfb36c750bc5561b3
Matt Kruszewski <m@ttkrusz.com>
2017-12-07 11:11:09 -0700

    base_settings must account for new directory

--- eggwords/eggwords/settings/base_settings.py
+++ eggwords/eggwords/settings/base_settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-REACT_APP_DIR = os.path.join(BASE_DIR, '../frontend')
+REACT_APP_DIR = os.path.join(BASE_DIR, '../../frontend')
 
 STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'build', 'static'),
